b436dce1df461ecdd62387d9bbdf7674343a794f
Ali_545 <70983185+AliS45@users.noreply.github.com>
2022-09-16 21:49:26 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,43 @@
+Config = {}
+
+
+Config.Jobs = {
+    ["police"] = {
+        coords      = vector3(-71.56, -817.6, 326.16),
+
+        outfitName  = "testing2",
+
+        job         = "police",
+        model       = "mp_m_freemode_01",
+        grade       = 'recruit',
+        arms        = {component_id = 3, texture = 0, drawable = 0},
+        torso       = {component_id = 11, texture = 0, drawable = 93},
+        undershirt  = {component_id = 8, texture = 0, drawable = 15},
+        pants       = {component_id = 4, texture = 2, drawable = 87},
+        shoes       = {component_id = 6, texture = 0, drawable = 25},
+        bag         = {component_id = 5, texture = 6, drawable = 281},
+        accesories  = {component_id = 7, texture = 0, drawable = 0},
+        kevlar      = {component_id = 9, texture = 0, drawable = 59},
+        badge       = {component_id = 10, texture = 0, drawable = 0},
+        hat         = {component_id = 0, texture = 0, drawable = -1}
+    },
+    ["mechanic"] = {
+        coords      = vector3(-79.68, -817.76, 326.16),
+
+        outfitName  = "testing1",
+
+        job         = "mechanic",
+        model       = "mp_m_freemode_01",
+        grade       = 'recrue',
+        arms        = {component_id = 3, texture = 0, drawable = 0},
+        torso       = {component_id = 11, texture = 0, drawable = 21},
+        undershirt  = {component_id = 8, texture = 0, drawable = 15},
+        pants       = {component_id = 4, texture = 2, drawable = 45},
+        shoes       = {component_id = 6, texture = 0, drawable = 2},
+        bag         = {component_id = 5, texture = 6, drawable = 12},
+        accesories  = {component_id = 7, texture = 0, drawable = 0},
+        kevlar      = {component_id = 9, texture = 0, drawable = 59},
+        badge       = {component_id = 10, texture = 0, drawable = 0},
+        hat         = {component_id = 0, texture = 0, drawable = -1}
+    }
+}
